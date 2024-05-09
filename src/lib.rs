117109ca0a7974a48a7a98e71b95ea81e1533b02
bjmayor <415074476@qq.com>
2024-05-09 23:39:51 +0800

    feature: support EnumFrom macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,46 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 // proc macro crate
+// for enum, we'd like to generate From impls for each variant
+#[proc_macro_derive(EnumFrom)]
+pub fn derive_enum_from(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    // get the ident
+    let name = input.ident;
+    // get enum variants
+    let variants = match input.data {
+        syn::Data::Enum(data) => data.variants,
+        _ => panic!("EnumFrom only works with enums"),
+    };
+    // for each variant, get the ident and fields
+    let from_impls = variants.iter().map(|variant| {
+        let ident = &variant.ident;
+        match &variant.fields {
+            syn::Fields::Unit => quote! {},
+            syn::Fields::Named(_fields) => quote! {},
+            syn::Fields::Unnamed(fields) => {
+                // only support one field
+                if fields.unnamed.len() != 1 {
+                    quote! {}
+                } else {
+                    let field = fields.unnamed.first().expect("should have 1 field");
+                    let ty = &field.ty;
+                    quote! {
+                         impl From<#ty> for #name {
+                            fn from(v: #ty) -> Self {
+                                    Self::#ident(v)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+    // quote return proc-macro2 TokenTtream so we need to convert it to TokenStream
+    quote! {
+        #(#from_impls)*
+    }
+    .into()
+}
